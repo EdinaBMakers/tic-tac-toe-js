f3fcccad787de23b99d6451a54c9ff828aeb283b
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-12-08 20:09:26 +0000

    Refactor tests

--- tests/game.test.js
+++ tests/game.test.js
@@ -3,28 +3,26 @@
 const Game = require('../src/game');
 
 describe('Game', () => {
+  let game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
   describe('Defaults', () => {
     test('Game is not over by default', () => {
-      const game = new Game();
-
       expect(game.isOver).toBe(false);
     });
 
     test('Game does not have a winner by default', () => {
-      const game = new Game();
-
       expect(game.winner).toBe(null);
     });
 
     test('X is the first player', () => {
-      const game = new Game();
-
       expect(game.nextPlayer).toBe('X');
     });
 
     test('Board is empty by default', () => {
-      const game = new Game();
-
       expect(game.board).toStrictEqual([null, null, null, null, null, null, null, null, null]);
     });
   });
